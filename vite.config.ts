1ce554344b0d9348b4eaa5d6c9e1109923212558
Priyanshu Kumar <singhpriansh.com@gmail.com>
2026-06-22 20:33:38 +0530

    Max chunks resized

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  build: { outDir: 'build' },
+  build: { 
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   plugins: [react(), tailwindcss()],
 })
