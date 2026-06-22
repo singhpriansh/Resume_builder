c1dd6978964c2825d7c3f13ea4dca2263573872b
Priyanshu Kumar <singhpriansh.com@gmail.com>
2026-06-22 21:46:51 +0530

    Changed some settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 10000, 
-    outDir: 'build',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
+    // chunkSizeWarningLimit: 10000, 
+    outDir: 'dist',
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         return 'vendor';
+    //       }
+    //     }
+    //   }
+    // }
   },
   plugins: [react(), tailwindcss()],
 })
